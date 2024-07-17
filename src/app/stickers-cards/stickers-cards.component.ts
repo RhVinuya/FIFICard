9a40033a30a7e998939c0dee910afc7422b1e1ec
Nelson Froilan Escuro <110151312+TTI-nelson@users.noreply.github.com>
2024-07-17 23:31:24 +0800

    sticker-cards-fix

--- src/app/stickers-cards/stickers-cards.component.ts
+++ src/app/stickers-cards/stickers-cards.component.ts
@@ -1,20 +1,19 @@
-import { Card } from './../models/card';
-import { EventService } from './../services/event.service';
-import { CardService } from './../services/card.service';
-import { ActivatedRoute } from '@angular/router';
-import { Title } from '@angular/platform-browser';
-import { PriceService } from './../services/price.service';
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { LoadingController } from '@ionic/angular';
-import { Event } from '../models/event';
+import { Card } from "./../models/card";
+import { EventService } from "./../services/event.service";
+import { CardService } from "./../services/card.service";
+import { ActivatedRoute } from "@angular/router";
+import { Title } from "@angular/platform-browser";
+import { PriceService } from "./../services/price.service";
+import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
+import { LoadingController } from "@ionic/angular";
+import { Event } from "../models/event";
 
 @Component({
-  selector: 'app-stickers-cards',
-  templateUrl: './stickers-cards.component.html',
-  styleUrls: ['./stickers-cards.component.scss']
+  selector: "app-stickers-cards",
+  templateUrl: "./stickers-cards.component.html",
+  styleUrls: ["./stickers-cards.component.scss"],
 })
 export class StickersCardsComponent implements OnInit {
-  
   loadingController: LoadingController;
   priceService: PriceService;
   eventService: EventService;
@@ -24,7 +23,7 @@ export class StickersCardsComponent implements OnInit {
     _loadingController: LoadingController,
     _priceService: PriceService,
     _eventService: EventService,
-    _cardService: CardService,
+    _cardService: CardService
   ) {
     this.loadingController = _loadingController;
     this.priceService = _priceService;
@@ -32,8 +31,8 @@ export class StickersCardsComponent implements OnInit {
     this.cardService = _cardService;
   }
 
-  original: Card[] = []
-  cards: Card[] = []
+  original: Card[] = [];
+  cards: Card[] = [];
   allEvents: Event[] = [];
   occassions: Event[] = [];
   others: Event[] = [];
@@ -47,17 +46,16 @@ export class StickersCardsComponent implements OnInit {
   async initalize() {
     let loading: HTMLIonLoadingElement;
     loading = await this.loadingController.create({
-      message: 'Loading...'
+      message: "Loading...",
     });
     await loading.present();
     try {
       this.original = await this.cardService.getCardsByType("sticker");
       this.cards = this.original;
-      this.allEvents = await  this.eventService.getEventSticker();
-      this.occassions = this.allEvents.filter(x => x.tag === 'Occasions');
-      this.others = this.allEvents.filter(x => x.tag === 'Others');
-    }
-    finally {
+      this.allEvents = await this.eventService.getEventSticker();
+      this.occassions = this.allEvents.filter((x) => x.tag === "Occasions");
+      this.others = this.allEvents.filter((x) => x.tag === "Others");
+    } finally {
       await loading.dismiss();
     }
   }
@@ -65,32 +63,33 @@ export class StickersCardsComponent implements OnInit {
   async setEvent(id: string) {
     let loading: HTMLIonLoadingElement;
     loading = await this.loadingController.create({
-      message: 'Loading...'
+      message: "Loading...",
     });
     await loading.present();
-    try{
-      if (id === 'all'){
+    try {
+      if (id === "all") {
         this.cards = this.original;
         this.title = "ALL";
-      }
-      else {
+      } else {
         this.cards = [];
-        let event: Event | undefined = this.allEvents.find(x => x.id === id);
-        if (event){
-          this.title = event.title && event.title !== '' ? event.title.toUpperCase() : event.name!.toUpperCase();
-          this.original.forEach(card => {
-            if (card.events!.findIndex(x => x === event!.name) >= 0) this.cards.push(card);
-          })
+        let event: Event | undefined = this.allEvents.find((x) => x.id === id);
+        if (event) {
+          this.title =
+            event.title && event.title !== ""
+              ? event.title.toUpperCase()
+              : event.name!.toUpperCase();
+          this.original.forEach((card) => {
+            if (card.events!.findIndex((x) => x === event!.name) >= 0)
+              this.cards.push(card);
+          });
         }
       }
-    }
-    finally{
+    } finally {
       await loading.dismiss();
-      const element = document.getElementById('items');
+      const element = document.getElementById("items");
       if (element != null) {
-        element.scrollIntoView({ behavior: 'smooth' });
-     }
-
+        element.scrollIntoView({ behavior: "smooth" });
+      }
     }
   }
 }
