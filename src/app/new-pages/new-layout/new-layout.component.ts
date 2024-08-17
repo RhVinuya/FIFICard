b12047d074fe5a8aa3e6405ef463a6c6b2dde598
RhVinuya <51703645+RhVinuya@users.noreply.github.com>
2024-08-17 14:32:17 +0800

    Login and Registration
    Log account indicator
    Registration page

--- src/app/new-pages/new-layout/new-layout.component.ts
+++ src/app/new-pages/new-layout/new-layout.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonContent } from '@ionic/angular';
-import { NgbPopover } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-new-layout',
--- src/app/new-pages/new-registration/new-registration.component.ts
+++ src/app/new-pages/new-registration/new-registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-registration',
@@ -9,7 +10,27 @@ export class NewRegistrationComponent implements OnInit {
 
   constructor() { }
 
+  form = new FormGroup({
+    email: new FormControl<string>('', [Validators.required, Validators.email]),
+    firstname: new FormControl<string>('', [Validators.required]),
+    lastname: new FormControl<string>('', [Validators.required]),
+    birthday: new FormControl<Date | null>(null, [Validators.required]),
+    password: new FormControl<string>('', [Validators.required, Validators.minLength(5)]),
+    confirm: new FormControl<string>('', [Validators.required, Validators.minLength(5)]),
+    newsletter: new FormControl<boolean>(true),
+    terms: new FormControl<boolean>(false),
+  });
+
+  submitted: boolean = false;
+  processing: boolean = false;
+
   ngOnInit(): void {
+  }
+
+  submit(){
+    this.submitted = true;
+    if (this.form.invalid) return;
+    this.processing = true;
   }
 
 }
