6d3e77c5981cfe67a7c3b2faa890d1a5a166d99e
RhVinuya <51703645+RhVinuya@users.noreply.github.com>
2024-09-03 16:09:20 +0800

    icon count

--- src/app/new-pages/new-layout/new-layout-header/new-layout-header.component.ts
+++ src/app/new-pages/new-layout/new-layout-header/new-layout-header.component.ts
@@ -1,8 +1,13 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { timer } from 'rxjs';
 import { LocationType } from 'src/app/new-models/new-enum';
 import { INewUser } from 'src/app/new-models/new-user';
+import { NewCartService } from 'src/app/new-services/new-cart.service';
 import { NewLocationService } from 'src/app/new-services/new-location.service';
+import { NewPersonalizeService } from 'src/app/new-services/new-personalize.service';
+import { NewStorageService } from 'src/app/new-services/new-storage.service';
+import { NewWishlistService } from 'src/app/new-services/new-wishlist.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -27,13 +32,22 @@ export class NewLayoutHeaderComponent implements OnInit {
   @Output() onShowMenu: EventEmitter<boolean> = new EventEmitter();
 
   locationService: NewLocationService;
+  wishlistService: NewWishlistService;
+  cartService: NewCartService;
+  personalizeService: NewPersonalizeService;
   router: Router;
 
   constructor(
     _locationService: NewLocationService,
+    _wishlistService: NewWishlistService,
+    _cartService: NewCartService,
+    _personalizeService: NewPersonalizeService,
     _router: Router
   ) { 
     this.locationService = _locationService;
+    this.wishlistService = _wishlistService;
+    this.cartService = _cartService;
+    this.personalizeService = _personalizeService;
     this.router = _router
   }
 
@@ -46,10 +60,31 @@ export class NewLayoutHeaderComponent implements OnInit {
   menuType: 'stickers' | 'postcards' | 'gifts' | undefined;
   hideDivTimeout: any;
   eventpriorities = environment.eventpriorities;
+  wishlistCountDisplay = '';
+  cartCountDisplay = '';
+  projectCountDisplay = '';
 
   ngOnInit(): void {
     this.location = this.locationService.getlocation();
     this.logo = this.locationService.getLogo();
+
+    timer(100, 500).subscribe(async time => {
+
+      let count = this.wishlistService.get().length;
+      if (count > 0 && count <= 9) this.wishlistCountDisplay = count.toString();
+      else if (count > 9) this.wishlistCountDisplay = '9+';
+      else this.wishlistCountDisplay = '';
+
+      count = (await this.cartService.getAll()).length;
+      if (count > 0 && count <= 9) this.cartCountDisplay = count.toString();
+      else if (count > 9) this.cartCountDisplay = '9+';
+      else this.cartCountDisplay = '';
+
+      count = (await this.personalizeService.getAll()).length;
+      if (count > 0 && count <= 9) this.projectCountDisplay = count.toString();
+      else if (count > 9) this.projectCountDisplay = '9+';
+      else this.projectCountDisplay = '';
+    });
   }
 
   onHover(menu: 'stickers' | 'postcards' | 'gifts' | undefined) {
