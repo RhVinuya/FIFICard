7c606f6bfe568c640fdea70098f9af0aba579941
Nelson Froilan Escuro <110151312+TTI-nelson@users.noreply.github.com>
2024-09-12 20:18:06 +0800

    feature: add card column parameter

--- src/app/components-mobile/categories/card-categories-mobile/card-categories-mobile.component.ts
+++ src/app/components-mobile/categories/card-categories-mobile/card-categories-mobile.component.ts
@@ -1,40 +1,41 @@
-import { EventService } from './../../../services/event.service';
-import { NewCardService } from 'src/app/new-services/new-card.service';
-import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { INewCard } from 'src/app/new-models/new-card';
-import { NewEventService } from 'src/app/new-services/new-event.service';
-import { NewEvent } from 'src/app/new-models/new-event';
+import { EventService } from "./../../../services/event.service";
+import { NewCardService } from "src/app/new-services/new-card.service";
+import { Component, OnInit } from "@angular/core";
+import { environment } from "src/environments/environment";
+import { INewCard } from "src/app/new-models/new-card";
+import { NewEventService } from "src/app/new-services/new-event.service";
+import { NewEvent } from "src/app/new-models/new-event";
 
 @Component({
-  selector: 'app-card-categories-mobile',
-  templateUrl: './card-categories-mobile.component.html',
-  styleUrls: ['./card-categories-mobile.component.scss']
+  selector: "app-card-categories-mobile",
+  templateUrl: "./card-categories-mobile.component.html",
+  styleUrls: ["./card-categories-mobile.component.scss"],
 })
 export class CardCategoriesMobileComponent implements OnInit {
-
   eventService: NewEventService;
+  columns: number = 2;
 
   cardevents = environment.cardevents;
 
   activeevents: string[] = [];
   events: NewEvent[] = [];
 
-  constructor(
-    _eventService: NewEventService
-  ) {
+  constructor(_eventService: NewEventService) {
     this.eventService = _eventService;
   }
 
   ngOnInit(): void {
-    this.load().then( () => {
-      console.log('card categories loaded');
-    })
+    this.load().then(() => {
+      console.log("card categories loaded");
+    });
   }
 
   async load() {
-    this.events = await this.eventService.getEventByType('card');
+    this.events = await this.eventService.getEventByType("card");
     console.log(this.events);
   }
 
+  getColumnSize() {
+    return 12 / this.columns;
+  }
 }
